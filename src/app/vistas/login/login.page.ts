f9c1ba78d760d13babe2b48e8d6a8625f44746da
alex <alex@fabrizzio.cl>
2023-09-08 19:33:00 -0300

    login simple

--- src/app/vistas/login/login.page.ts
+++ src/app/vistas/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  email : string = "";
+  contrasena : string = "";
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+  
+  onEnviarFormulario(){
+    if(this.email.toUpperCase() == "PGY4121-001D" && this.contrasena.toUpperCase() == "PGY4121-001D"){
+      this.router.navigateByUrl('feed')
+    }else{
+      alert("Contra o user incorrecto.");
+    }
+    
   }
 
 }
